b673ac464d79c179d439c9859a353cc0098c045a
Arian van Putten <aeroboy94@gmail.com>
2015-08-24 23:11:27 +0200

    second commit

--- provider.go
+++ provider.go
@@ -6,8 +6,24 @@ import (
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{
-			"scaleway_server": resourceServer(),
+		Schema: map[string]*schema.Schema{
+			"token" & schema.Schema{},
 		},
+		ResourcesMap: map[string]*schema.Resource{
+			"scaleway_organization":   organizationResource,
+			"scaleway_server":         serverResource,
+			"scaleway_volume":         volumeResource,
+			"scaleway_image":          imageResource,
+			"scaleway_ip":             ipResource,
+			"scaleway_security_group": scalewaySecurityGroup,
+		},
+		ConfigureFunc: providerConfigure,
 	}
+}
+
+func providerFunc(d *schema.ResourceData) (interface{}, error) {
+	config := Config{
+		Token: d.Get("token")(string),
+	}
+	return config.Client()
 }
